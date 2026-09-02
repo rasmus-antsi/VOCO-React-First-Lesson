f2b9edad7203c58b8c49584eb0db3c3af839568f
rasmus <rasmus435@icloud.com>
2026-09-02 11:04:57 +0300

    DateBox bugfix

--- src/components/DateBox.jsx
+++ src/components/DateBox.jsx
@@ -5,9 +5,9 @@ import './DateBox.css'
 function DateBox(props) {
   return (
     <div className="date-box">
-      <span ClassName="date-box__month">{props.month}</span>
+      <span className="date-box__month">{props.month}</span>
       <span className="date-box__day">{props.day}</span>
-      <span ClassName="date-box__year">{props.year}</span>
+      <span className="date-box__year">{props.year}</span>
     </div>
   )
 }
