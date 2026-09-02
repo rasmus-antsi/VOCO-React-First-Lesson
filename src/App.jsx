e73a348ed2ac686421a8946e85cd48d4b6e0482c
rasmus <rasmus435@icloud.com>
2026-09-02 10:57:16 +0300

    second productCard

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import ProductCard from './components/ProductCard.jsx'
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <ProductCard month="January" day="10" year="2023" title="New book" amount={30.99} />
-    </div>
+		<ProductCard month="January" day="10" year="2023" title="New book" amount={30.99} />
+  		<ProductCard month="January" day="10" year="2023" title="New jeans" amount={99.99} />
+     </div>
   )
 }
 
